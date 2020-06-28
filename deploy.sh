1677984e285f995cf5b5656b07861fba40bd7408
yufeng-ltx <yufeng.ltx@gmail.com>
2020-06-29 00:06:29 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env sh
 # 确保脚本抛出遇到的错误
 set -e
-npm run build # 生成静态文件
-cd docs/.vuepress/dist # 进入生成的文件夹
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
 
 # deploy to github
-if [ -z "$GITHUB_TOKEN" ]; then
+if [ -z "$GITHUB_TOKEN" ]; then #判断是否存在token
   msg='deploy'
   githubUrl=git@github.com:yufeng-ltx/yufeng-ltx.github.io.git
 else
-  msg='来自github action的自动部署'
+  msg='github action deploy'
   githubUrl=https://yufeng-ltx:${GITHUB_TOKEN}@github.com/yufeng-ltx/yufeng-ltx.github.io.git
   git config --global user.name "yufeng-ltx"
   git config --global user.email "yufeng.ltx@gmail.com"
@@ -17,7 +21,7 @@ fi
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master # 推送到github
+git push -f $githubUrl master # publish github
 
 cd -
 rm -rf docs/.vuepress/dist
